0715ce508e60a969dfc865e649afca705a71ddad
douglaspnt <161038707+douglaspnt@users.noreply.github.com>
2024-04-18 14:15:31 -0300

    CRUD-Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,31 @@
+package com.generation.farmacia.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo jogo é obrigatório!")
+	@Size(min = 5, max = 50, message = "O atributo tipo deve conter no mínimo 05 e no máximo 50 caracteres")
+	private String tipo;
+	
+	
+}
